3f998805938e9cc84ddfb0c800ec38f41baa4a0c
Moabe Renato <mrov@cin.ufpe.br>
2022-07-10 11:04:23 -0300

    detailing the scenario for remove new item

--- src/app/todo-list/todo-list.component.spec.ts
+++ src/app/todo-list/todo-list.component.spec.ts
@@ -48,7 +48,6 @@ describe('TodoListComponent', () => {
 
     const todoInput: HTMLInputElement = fixture.debugElement.query( By.css('#todoInput') ).nativeElement;
 
-    
     todoInput.value = 'Estudar para fisica';
     todoInput.dispatchEvent(new Event('input'));
     
@@ -63,34 +62,48 @@ describe('TodoListComponent', () => {
     expect(component.todoList.length).toBeGreaterThan(1);
   });
 
-  it('should delete a item in the todoList', () => {
+  it('should add and delete a item in the todoList', () => {
+    // Create the spies for the component functions
     const addTodoItemSpy = spyOn(component, "addTodoItem").and.callThrough();
     const deleteTodoItemSpy = spyOn(component, "deleteTodoItem").and.callThrough();
 
-
-    const addItemButton = fixture.debugElement.query( By.css('#addTodoItem') ).nativeElement;
+    // Search the input and addItem button in the HTML
+    const addItemButton: HTMLButtonElement = fixture.debugElement.query( By.css('#addTodoItem') ).nativeElement;
     const todoInput: HTMLInputElement = fixture.debugElement.query( By.css('#todoInput') ).nativeElement;
 
-    
+    // Simulate user input in the form
     todoInput.value = 'testetodo';
     todoInput.dispatchEvent(new Event('input'));
     
     fixture.detectChanges();
-
+    
+    // Simulate user click in the add button
     addItemButton.dispatchEvent(new Event('click'));
 
     fixture.detectChanges();
+
+    // Search the item was added in the todoList
+    let findRecentlyAddedItem = component.todoList.find(item => { return item.description === "testetodo"});
     
+    // Expects
     expect(addTodoItemSpy).toHaveBeenCalled();
+    expect(findRecentlyAddedItem).toBeTruthy();
     expect(component.newTodoItem.description).toBeFalsy();
     expect(component.todoList.length).toBeGreaterThan(1);
 
+    // Search the delete button in the HTML by css ID
     const deleteItemButton = fixture.debugElement.query( By.css('#delete-item-1') ).nativeElement;
 
+    // Simulate the user click in the delete button
     deleteItemButton.dispatchEvent(new Event('click'));
 
     fixture.detectChanges();
 
+    // Search the item was added in the todoList
+    findRecentlyAddedItem = component.todoList.find(item => { return item.description === "testetodo"});
+
+    // Expects
+    expect(findRecentlyAddedItem).toBeFalsy();
     expect(deleteTodoItemSpy).toHaveBeenCalledOnceWith(1);
     expect(component.todoList.length).toEqual(1);
   });
